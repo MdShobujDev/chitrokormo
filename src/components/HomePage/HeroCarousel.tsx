f4a6ed4ef3d0aca845158481baf4c927f4bd3fb4
Md Shobuj Mia <md.shobuj.developer@gmail.com>
2025-01-16 14:52:47 +0600

    fix: remove contentStyle from heroCarousel

--- src/components/HomePage/HeroCarousel.tsx
+++ src/components/HomePage/HeroCarousel.tsx
@@ -8,15 +8,6 @@ import { Carousel } from "antd";
 import Image from "next/image";
 import React from "react";
 
-const contentStyle: React.CSSProperties = {
-  margin: 0,
-  height: "160px",
-  color: "#fff",
-  lineHeight: "160px",
-  textAlign: "center",
-  background: "#364d79",
-};
-
 const HeroCarousel: React.FC = () => {
   const onChange = (currentSlide: number) => {
     console.log(currentSlide);
