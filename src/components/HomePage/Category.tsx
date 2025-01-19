5a2e93e7420f54a25980e62d9a65fb30d11375b5
Md Shobuj Mia <md.shobuj.developer@gmail.com>
2025-01-19 19:45:21 +0600

    updated code

--- src/components/HomePage/Category.tsx
+++ src/components/HomePage/Category.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import EmblaCarousel from "../ui/EmblaCarouel";
 
 const categoryItems = [
   {
@@ -91,28 +90,25 @@ const Category = () => {
         <h1 className=" sm:text-2xl text-xl  font-bold text-primary">
           ক্যাটাগরি অনুযায়ী কেনাকাটা করুন
         </h1>
-        <div className=" sm:pt-5 pt-3">
-          <EmblaCarousel autoplay>
-            {categoryItems.map((item) => (
-              <div
-                key={item.id}
-                className="[flex:0_0_55%] min-[400px]:[flex:0_0_47%]  min-[500px]:[flex:0_0_38%] sm:[flex:0_0_30%] min-[720px]:[flex:0_0_25%] lg:[flex:0_0_18.7%] flex flex-col justify-between gap-1 rounded-lg  overflow-hidden relative cursor-pointer select-none bg-[#F3F3F3] p-2"
-              >
+        <div className=" sm:pt-5 pt-3 flex sm:gap-2 gap-1 flex-wrap justify-center items-center">
+          {categoryItems.map((item) => (
+            <div className=" sm:w-28 w-16 flex flex-col justify-between gap-1 rounded-lg  overflow-hidden relative cursor-pointer select-none bg-[#F3F3F3] sm:p-2 p-1 group/category">
+              <div className=" bg-red-300 rounded-lg overflow-hidden ">
                 <Image
                   src={item.image}
                   alt={item.title}
                   width={500}
                   height={500}
-                  className=" rounded-lg aspect-[7/5]"
+                  className="  aspect-[7/6] group-hover/category:scale-[1.05] transition-all duration-300 ease-linear "
                 />
-                <div>
-                  <h2 className=" md:text-lg sm:text-base text-sm text-center">
-                    {item.title}
-                  </h2>
-                </div>
               </div>
-            ))}
-          </EmblaCarousel>
+              <div>
+                <h2 className=" sm:text-xs text-[6px] text-center">
+                  {item.title}
+                </h2>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -17,7 +17,7 @@ interface ProductData {
 
 const ProductCard = ({ data }: { data: ProductData }) => {
   return (
-    <div className=" bg-[#F3F3F3] p-2 rounded-lg  flex flex-col justify-between gap-3 shadow group/product hover:scale-[1.02] transition-all duration-200 ease-linear">
+    <div className=" bg-[#F3F3F3] p-2 rounded-lg  flex flex-col justify-between sm:gap-3 gap-2 shadow group/product hover:scale-[1.02] transition-all duration-200 ease-linear">
       {/* image use here  */}
       <div className=" relative overflow-hidden rounded-lg">
         <Image
@@ -27,29 +27,29 @@ const ProductCard = ({ data }: { data: ProductData }) => {
           height={500}
           className=" rounded-lg aspect-[7/5] group-hover/product:scale-[1.05] transition-all duration-200 ease-linear"
         />
-        <div className=" absolute top-2 left-2 bg-primary px-1.5 py-0.5  rounded-2xl font-bold text-xs text-white max-w-max">
+        <div className=" absolute top-2 left-2 bg-primary px-1.5 py-0.5  rounded-2xl font-bold sm:text-xs text-[10px]  text-white max-w-max">
           save 5%
         </div>
         <div className="absolute top-2 right-2 cursor-pointer bg-white p-0.5 rounded hover:bg-primary hover:text-white transition-all duration-150 ease-in md:group-hover/product:block md:hidden">
           <IoCartOutline size={18} />
         </div>
       </div>
-      <div className=" flex flex-col gap-2">
-        <h2 className=" text-gray-800 line-clamp-1">
+      <div className=" flex flex-col sm:gap-2 gap-1">
+        <h2 className=" text-gray-800 line-clamp-1 sm:text-base text-sm">
           ডার্ক মোড ওয়ালপেপার ডার্ক মোড ওয়ালপেপারডার্ক মোড ওয়ালপেপার
         </h2>
         <div className=" flex items-center gap-2">
           <div className=" flex items-center text-primary">
-            <span>
-              <TbCurrencyTaka size={30} />
+            <span className=" text-2xl sm:text-3xl">
+              <TbCurrencyTaka />
             </span>
-            <h2 className=" mt-[2px] text-2xl font-bold ">250</h2>
+            <h2 className=" mt-[2px] sm:text-2xl text-xl font-bold ">250</h2>
           </div>
-          <div className=" flex items-center">
+          <div className=" flex items-center text-gray-600">
             <span>
               <TbCurrencyTaka size={15} />
             </span>
-            <h2 className=" mt-[2px] line-through font-light text-gray-700">
+            <h2 className=" mt-[2px] line-through font-light  sm:text-base text-sm">
               299
             </h2>
           </div>
@@ -64,7 +64,7 @@ const ProductCard = ({ data }: { data: ProductData }) => {
           </div>
 
           <Link
-            className="md:text-sm text-xs hover:bg-black bg-primary text-white max-w-max sm:px-3 px-2 py-[4px] rounded-2xl  transition-all ease-linear duration-200"
+            className="md:text-sm sm:text-xs text-[10px] hover:bg-black bg-primary text-white max-w-max sm:px-3 px-2 py-[4px] rounded-2xl  transition-all ease-linear duration-200"
             href={data.link}
           >
             <span>এখনই কিনুন</span>
