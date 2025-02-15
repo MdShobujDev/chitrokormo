b0f9e587112cc02a5beb0988f3212035bcd6ab56
Md Shobuj Mia <md.shobuj.developer@gmail.com>
2025-02-15 20:20:03 +0600

    fix: solve error

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,24 +1,27 @@
+import { Suspense } from "react";
 import SideBar from "./_components/Products/products_layout/SideBar";
 import TopBar from "./_components/Products/products_layout/TopBar";
 import ProductsBody from "./_components/Products/ProductsBody";
 
 const Products = () => {
   return (
-    <div className="max-w-7xl mx-auto px-5">
-      <div>
+    <Suspense fallback={<div>Loading pagination...</div>}>
+      <div className="max-w-7xl mx-auto px-5">
         <div>
-          <TopBar />
-        </div>
-        <div className=" flex py-5 gap-4 ">
-          <div className=" min-[950px]:block hidden w-full flex-[0_0_25%]">
-            <SideBar />
+          <div>
+            <TopBar />
           </div>
-          <div className=" w-full min-[950px]:flex-[0_0_75%]">
-            <ProductsBody />
+          <div className=" flex py-5 gap-4 ">
+            <div className=" min-[950px]:block hidden w-full flex-[0_0_25%]">
+              <SideBar />
+            </div>
+            <div className=" w-full min-[950px]:flex-[0_0_75%]">
+              <ProductsBody />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
