030011912ea9b4db8075969361c563d88773c88b
Md Shobuj Mia <md.shobuj.developer@gmail.com>
2025-03-16 21:00:37 +0600

    fix : solve all error

--- src/app/login/_components/LoginForm.tsx
+++ src/app/login/_components/LoginForm.tsx
@@ -3,7 +3,7 @@
 import { signIn } from "next-auth/react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import styled from "styled-components";
 
 const Input = styled.input`
@@ -40,36 +40,38 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="sm:w-[450px] w-full flex flex-col items-center justify-center bg-white shadow p-5 rounded-lg">
-      <h2 className="text-2xl text-primary font-medium mb-3">Login</h2>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <form onSubmit={handleLogin} className="flex flex-col gap-3 w-full">
-        <Input
-          type="email"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button
-          className="bg-primary p-2.5 rounded text-white w-full hover:bg-primary/90"
-          type="submit"
-        >
-          Login
-        </button>
-      </form>
-      <Link href="/forgot-password" className="text-primary mt-2">
-        Forgot Password?
-      </Link>
-      <div className="flex items-center justify-center gap-2 mt-3">
-        <p>{`Don't have an account ?`}</p>
-        <Link href="/register" className="text-primary">
-          Register Now
+    <Suspense fallback={<p>Loading...</p>}>
+      <div className="sm:w-[450px] w-full flex flex-col items-center justify-center bg-white shadow p-5 rounded-lg">
+        <h2 className="text-2xl text-primary font-medium mb-3">Login</h2>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <form onSubmit={handleLogin} className="flex flex-col gap-3 w-full">
+          <Input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="bg-primary p-2.5 rounded text-white w-full hover:bg-primary/90"
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+        <Link href="/forgot-password" className="text-primary mt-2">
+          Forgot Password?
         </Link>
+        <div className="flex items-center justify-center gap-2 mt-3">
+          <p>{`Don't have an account ?`}</p>
+          <Link href="/register" className="text-primary">
+            Register Now
+          </Link>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import Category from "@/app/_components/Category";
 import ForYou from "@/app/_components/ForYou";
 import HeroCarousel from "@/app/_components/HeroCarousel";
 import FeatureSection from "@/components/FeatureSection";
+import { Suspense } from "react";
 import FlashSale from "./_components/Flash Sale/FlashSale";
 import TopProduct from "./_components/Top Products/TopProduct";
 import TrendingProducts from "./_components/TrendingProducts";
 
 export default function Home() {
   return (
-    <>
+    <Suspense fallback={<p>Loading...</p>}>
       <main>
         <HeroCarousel />
         <Category />
@@ -18,6 +19,6 @@ export default function Home() {
         <ForYou />
         <FeatureSection />
       </main>
-    </>
+    </Suspense>
   );
 }
