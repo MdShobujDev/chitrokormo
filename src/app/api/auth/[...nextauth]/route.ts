86717993e40cbd84660b351316ee8475d87a495e
Md Shobuj Mia <md.shobuj.developer@gmail.com>
2025-03-17 15:41:51 +0600

    fix : solve all error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { authOptions } from "../auth-option";
+import { authOptions } from "../../../../auth-option";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
